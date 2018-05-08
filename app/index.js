b5b60be1d927da1b7614608d830cbc9a122ab12a
Chris Spooner <cmspooner@gmail.com>
2018-05-08 12:47:39 -0400

    First Push

--- app/index.js
+++ app/index.js
@@ -4,38 +4,124 @@ import document from "document";
 import * as messaging from "messaging";
 
 import { preferences } from "user-settings";
+import { HeartRateSensor } from "heart-rate";
+import { today } from "user-activity";
+import { goals } from "user-activity";
+import { user } from "user-profile";
+import { display } from "display";
+import { me } from "appbit";
+
 
 import * as util from "./util";
+
+let hrm = new HeartRateSensor();
 
 console.log("App Started");
 
 clock.granularity = "minutes";
 
+const SETTINGS_TYPE = "cbor";
+const SETTINGS_FILE = "settings.cbor";
+
 let background = document.getElementById("background");
 let clockLabel = document.getElementById("clockLabel");
 let mainStripeL = document.getElementById("mainStripeL");
+let pinStripeL = document.getElementById("pinStripeL");
 let mainStripeR = document.getElementById("mainStripeR");
+let pinStripeR = document.getElementById("pinStripeR");
+
+let tach = document.getElementById("tach");
+
+let hrNeedle = document.getElementById("hrNeedle");
+//hrNeedle.groupTransform.rotate.angle = 110;
+
+let hrLabel = document.getElementById("hrLabel");
+//hrLabel.text = "193"
+let stepsLabel = document.getElementById("stepsLabel");
+//stepsLabel.text = "21,753 steps"
+
+let settings = loadSettings();
 
 // Message is received
 messaging.peerSocket.onmessage = evt => {
   console.log(`App received: ${JSON.stringify(evt)}`);  
   if (evt.data.key === "bgcolor" && evt.data.newValue) {
-    let color = JSON.parse(evt.data.newValue);
-    console.log(`Setting background color: ${color}`);
-    background.style.fill = color;
+    settings.bgColor = JSON.parse(evt.data.newValue);
+    console.log(`Setting background color: ${settings.bgColor}`);
+    setBgColor();
   }
   if (evt.data.key === "stripeColor" && evt.data.newValue) {
-    let color = JSON.parse(evt.data.newValue);
-    console.log(`Setting Stripe color: ${color}`);
-    mainStripeR.style.fill = color;
-    mainStripeL.style.fill = color;
+    settings.stripesColor = JSON.parse(evt.data.newValue);
+    console.log(`Setting Stripe color: ${settings.stripesColor}`);
+    setStripesColor();
+  }
+  if (evt.data.key === "pinStripeColor" && evt.data.newValue) {
+    settings.pinStripesColor = JSON.parse(evt.data.newValue);
+    console.log(`Setting Stripe color: ${settings.pinStripesColor}`);
+    setPinStripesColor();
+  }
+  if (evt.data.key === "stripesToggle" && evt.data.newValue) {
+    settings.stripes = JSON.parse(evt.data.newValue);
+    console.log(`Stripes: ${settings.stripes}`);
+    setStripesColor();
+  }
+  if (evt.data.key === "pinStripesToggle" && evt.data.newValue) {
+    settings.pinStripes = JSON.parse(evt.data.newValue);
+    console.log(`Pin Stripes: ${settings.pinStripes}`);
+    setPinStripesColor();
   }
   if (evt.data.key === "clockColor" && evt.data.newValue) {
-    let color = JSON.parse(evt.data.newValue);
-    console.log(`Setting Clock color: ${color}`);
-    clockLabel.style.fill = color;
+    settings.clockColor = JSON.parse(evt.data.newValue);
+    console.log(`Setting Clock color: ${settings.clockColor}`);
+    setClockColor();
   }
+  if (evt.data.key === "tachColor" && evt.data.newValue) {
+    settings.tachColor = JSON.parse(evt.data.newValue);
+    console.log(`Setting Tach color: ${settings.clockColor}`);
+    setTachColor();
+  }
+  saveSettings();
 };
+
+function setBgColor(){
+  background.style.fill = settings.bgColor;
+}
+
+function setStripesColor(){
+  if (settings.stripes){
+    mainStripeR.style.fill = settings.stripesColor;
+    mainStripeL.style.fill = settings.stripesColor;
+  } else {
+    mainStripeR.style.fill = settings.bgColor;
+    mainStripeL.style.fill = settings.bgColor;
+  }
+}
+
+function setPinStripesColor(){
+  if (settings.pinStripes){
+    pinStripeR.style.fill = settings.pinStripesColor;
+    pinStripeL.style.fill = settings.pinStripesColor;
+  } else {
+    pinStripeR.style.fill = settings.bgColor;
+    pinStripeL.style.fill = settings.bgColor;
+  }
+}
+
+function setClockColor(){
+  clockLabel.style.fill = settings.clockColor;
+}
+
+function setTachColor(){
+  tach.style.fill = settings.tachColor;
+}
+
+function applySettings(){
+  setBgColor();
+  setStripesColor();
+  setPinStripesColor();
+  setClockColor();
+  setTachColor();
+}
 
 // Message socket opens
 messaging.peerSocket.onopen = () => {
@@ -46,6 +132,31 @@ messaging.peerSocket.onopen = () => {
 messaging.peerSocket.close = () => {
   console.log("App Socket Closed");
 };
+
+me.onunload = saveSettings;
+
+function loadSettings() {
+  console.log("Loading Settings!")
+  try {
+    return fs.readFileSync(SETTINGS_FILE, SETTINGS_TYPE);
+  } catch (ex) {
+    // Defaults
+    return {
+      bgColor : "deepskyblue",
+      stripesColor : "silver",
+      pinStripesColor : "black",
+      stripes : true,
+      pinStripes: true,
+      clockColor : "black",
+      tachColor : "black"
+    }
+  }
+}
+
+function saveSettings() {
+  console.log("Saving Settings");
+  settings.noFile = false;
+}
 
 function updateClock() {
   let today = new Date();
@@ -72,4 +183,47 @@ function updateClock() {
   clockLabel.text = `${hours}:${mins}`;
 }
 
+function updateClockData() {
+  if (display.on){
+    hrm.start();
+    let data = {
+        heart: {
+          theHeartRate: hrm.heartRate ? hrm.heartRate : 0
+        },
+        step: {
+          steps: today.adjusted.steps ? today.adjusted.steps: 0
+        }
+      };
+
+    //console.log("Data:");
+    //console.log(data.heart.theHeartRate);
+    //console.log(data.step.steps.toLocaleString());
+    
+    
+    if (data.heart.theHeartRate == 0) {
+      hrLabel.text = `--`;
+      hrNeedle.groupTransform.rotate.angle = -110;
+        
+    } else {
+      //console.log(user.restingHeartRate + ", " + user.age)
+      let min = parseInt(user.restingHeartRate);
+      let max = 220 - parseInt(user.age);
+      let degPerBpm = 192.5/(max-min);
+      //console.log(degPerBpm);
+      let angle = ((data.heart.theHeartRate - user.restingHeartRate) * degPerBpm) - 82.5;
+      //let angle = ((250 - user.restingHeartRate) * degPerBpm) - 82.5;
+      //console.log(angle);
+      hrNeedle.groupTransform.rotate.angle = angle ;
+      hrLabel.text = `${data.heart.theHeartRate}`;
+    }
+    
+    stepsLabel.text = `${data.step.steps.toLocaleString()} steps`;
+    hrm.stop()
+  }
+}
+
+setInterval(updateClockData, .1*1000);
 clock.ontick = () => updateClock();
+
+applySettings();
+hrm.start();
--- settings/index.jsx
+++ settings/index.jsx
@@ -11,18 +11,43 @@ function mySettings(props) {
             {color: "darkgreen"},
             {color: "cornsilk"},
             {color: "darkblue"},
-            {color: "black"}
+            {color: "#DED4C8"}, //champainish
+            {color: "powderblue"},
+            {color: "chocolate"},
+            {color: "black"},
+            {color: "darkorange"}
           ]}
         />
       </Section>
       <Section
         title={<Text bold align="center">Stripe Color</Text>}>
+        <Toggle
+          settingsKey="stripesToggle"
+          label="Stripes On/Off"
+        />
         <ColorSelect
           settingsKey="stripeColor"
           colors={[
             {color: "white"},
             {color: "black"},
-            {color: "silver"}
+            {color: "silver"},
+            {color: "crimson"},
+            {color: "lime"}
+          ]}
+        />
+      </Section>
+      <Section
+        title={<Text bold align="center">Pin Stripe Color</Text>}>
+        <Toggle
+          settingsKey="pinStripesToggle"
+          label="Pin Stripes On/Off"
+        />
+        <ColorSelect
+          settingsKey="pinStripeColor"
+          colors={[
+            {color: "white"},
+            {color: "black"},
+            {color: "crimson"}
           ]}
         />
       </Section>
@@ -32,9 +57,53 @@ function mySettings(props) {
           settingsKey="clockColor"
           colors={[
             {color: "white"},
-            {color: "black"}
+            {color: "black"},
+            {color: "crimson"}
           ]}
         />
+      </Section>
+      <Section
+        title={<Text bold align="center">Tach Color</Text>}>
+        <ColorSelect
+          settingsKey="tachColor"
+          colors={[
+            {color: "white"},
+            {color: "dimgrey"}
+          ]}
+        />
+      </Section>
+   <Section
+        title={<Text bold align="center">Contact Me</Text>}>
+        <Text>
+          Please don't hesitiate to contact me with questions or suggestions. This and all my other apps will always be free and Open Source. If you really like my app please considder buying me a coffee (or more likely electonic components that end up in my classroom). Thanks!
+        </Text>
+        <Link source="https://rawgit.com/cmspooner/Flashlight-App-for-FitBit-Ionic/master/settings/email.html">
+          <TextImageRow
+            label="Email"
+            sublabel="cmspooner@gmail.com"
+            icon="https://github.com/cmspooner/Flashlight-App-for-FitBit-Ionic/blob/master/resources/icons/settings/Email.png?raw=true"
+          />
+        </Link>
+        <Link source="https://github.com/cmspooner">
+          <TextImageRow
+            label="Github"
+            sublabel="https://github.com/cmspooner"
+            icon="https://github.com/cmspooner/Flashlight-App-for-FitBit-Ionic/blob/master/resources/icons/settings/Github.png?raw=true"
+          />
+        </Link>
+        <Link source="https://paypal.me/CMSpooner">
+          <TextImageRow
+            label="PayPal"
+            sublabel="cmspooner@gmail.com"
+            icon="https://github.com/cmspooner/Flashlight-App-for-FitBit-Ionic/blob/master/resources/icons/settings/Paypal.png?raw=true"
+          />
+        </Link>
+      </Section>
+      <Section
+        title={<Text bold align="center">Build Version and Notes</Text>}>
+        <Text>
+          1.0: First official release
+        </Text>
       </Section>
     </Page>
   );
